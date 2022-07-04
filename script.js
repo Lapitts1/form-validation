d0e0d56c357451c82de933cbc7729fd976de98f2
Toyalp <97072609+Toyalp@users.noreply.github.com>
2022-07-04 18:12:12 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// variables
+
+const id = (id) => document.getElementById(id);
+const classes = (classes) => document.getElementsByClassName(classes);
+
+const username = {
+      name: id("name"),
+      date: id("date"),
+      comment: id("comment"),
+      form: id("form"),
+      submit: id("submit")
+};
+
+
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+const comment = document.getElementById('comment');
+const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+
